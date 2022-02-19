f874384a5c07c1a7ac83709c9319c26044078149
Matteo Mosconi <matteo.mosconi.1999@gmail.com>
2022-02-19 12:33:19 +0100

    animations for player

--- Metroidvania-project/Assets/Scripts/PlayerController.cs
+++ Metroidvania-project/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     private bool isOnGround;
     public LayerMask whatIsGround;
 
+    public Animator anim;
+
 
 
     // Start is called before the first frame update
@@ -24,13 +26,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //move sideways
         TheRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, TheRB.velocity.y);
 
+        //handle direction change
+        if (TheRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (TheRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+
+
+        //check if on ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
 
+
+
+        //jumping
         if (Input.GetButtonDown("Jump") && isOnGround)
         {
             TheRB.velocity = new Vector2(TheRB.velocity.x, jumpForce);
         }
+
+
+
+
+
+
+
+
+        anim.SetBool("isonGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs( TheRB.velocity.x));
     }
 }
